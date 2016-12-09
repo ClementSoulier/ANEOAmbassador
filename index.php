300d4af97dc65d5b26a5c1296fa2feb6fe871dc8
TheNabo1 <soulier.clement@me.com>
2016-12-09 11:36:00 +0100

    ANEO Ambassador Website

---
+++ index.php
@@ -0,0 +1,231 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/jquery-3.1.0.js"></script>
+    <script src="js/jquery.color-2.1.0.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <!-- Header Row -->
+        <div class="col-md-12 text-center">
+            <img id="ambassador_logo" src="img/elements/AMABSSADOR_LOGO.png"/>
+            <h3 class="chubby">ORANGE IS THE NEW GAME</h3>
+        </div>
+    </div>
+    <br/><br/><br/><br/>
+    <div class="row">
+        <!-- Header Row -->
+        <div class="col-md-12 text-center">
+            <h2 style="margin:0!Important;" class="chubby">TU SOUHAITES DEVENIR</h2>
+            <h1 style="margin:-10!Important;" class="chubby orange">AMBASSADOR ANEO ?</h1>
+        </div>
+    </div>
+    <br/><br/><br/>
+    <div class="row">
+        <!-- Vidéo Row -->
+        <div class="col-md-12 text-center">
+            <div class="video-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/3Ej1xATIiuA" frameborder="0"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <br/><br/><br/><br/><br/>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="chubby">Qui est ANEO ?</h1>
+        </div>
+    </div>
+    <br/><br/>
+    <div class="darkgrey_background">
+        <div class="row">
+            <!-- Programme Row -->
+            <div class="col-md-4 col-md-offset-2">
+                <img class="img-responsive" src="img/elements/ANEO_LOGO-01.png" alt="ANEO"/>
+            </div>
+            <div class="col-md-4">
+                <h5 class="din orange_aneo"><strong>ANEO est un cabinet de conseil en Organisation et en
+                        Technologies.</strong></h5>
+                <div class="margin_top">
+                    <h5 class="din grey margin_left">Nous œuvrons pour un transition digitale positive pour l'humain
+                        !</h5>
+                    <h5 class="din grey margin_left">Notre approche consiste à transformer avec nos clients leurs
+                        intentions stratégiques en réalités quotidiennes et opérationnelles.</h5>
+                    <h5 class="din grey margin_left">Une seule méthode : HUMAN CENTRIC</h5>
+                    <h5 class="din grey margin_left">Convaincus que l'intélligence collective et l'épanouissement de nos
+                        collaborateurs favorisent la créativité et l'innovation, nous avons collectivement fait le choix d'une
+                        organisation à plat</h5>
+                </div>
+            </div>
+            <div class="col-md-7 col-md-offset-2">
+                <hr/>
+            </div>
+            <div class="col-md-2">
+                <h5 class="din margin_left"><a href="http://aneo.eu/" target="_blank">Site ANEO</a></h5>
+            </div>
+        </div>
+        <div class="row">
+
+        </div>
+    </div>
+    <br/><br/><br/><br/><br/>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="chubby">Programme<br/>Ambassador</h1>
+        </div>
+    </div>
+    <br/><br/>
+    <div>
+        <div class="row text-center">
+            <!-- Programme Row -->
+            <div id="bloctitle_programme_colleft" class="col-md-4 col-md-offset-2">
+                <h3 id="title_programme_colleft" class="din">C'est quoi</h3>
+            </div>
+            <div id="bloctitle_programme_colright" class="col-md-4">
+                <h3 id="title_programme_colright" class="din">Des privilèges</h3>
+            </div>
+            <div style="height: 5px;" class="col-md-8 col-md-offset-2 white_background">
+            </div>
+        </div>
+    </div>
+    <div class="margin_top">
+        <div class="row text-center">
+            <!-- Programme Row -->
+            <div style="height: 550px;" id="programme_colleft" class="col-md-4 col-md-offset-2">
+                <div class="arrow_box">
+                    <h4 class="chubby">Ambassador, c'est quoi ?</h4>
+                    <h5 class="din margin_top">Tout au long de l'année tu auras pour mission de développer la marque
+                        ANEO dans ton école, organiser des événements et participer à l'actualité d'ANEO sur ton campus.</h5>
+                    <h4 class="chubby margin_top">Quels sont les objectifs d'un ambassadeur ?</h4>
+                    <h5 class="din margin_top">Être le relais d'ANEO au sein de son école et incarner les valeurs
+                        d'ANEO.</h5>
+                    <h4 class="chubby margin_top">Qui peut devenir ambassadeur ?</h4>
+                    <h5 class="din margin_top">Tous les étudiants motivés dans une association étudiante.</h5>
+                </div>
+            </div>
+            <div style="height: 550px;" id="programme_colright" class="col-md-4">
+                <h4 class="chubby">Tu auras droit a quelques privileges</h4>
+                <h5 class="din margin_top">L'opportunité de participer à un séminaire d'intégration pour les ambassadeurs
+                    2017.</h5>
+                <h5 class="din">Une rémunération</h5>
+                <h5 class="din">Un accès à un réseau de profesionnels, pour tes stages et ta première embauche.</h5>
+                <h5 class="din">Un budget alloué pour organiser des événements ANEO dans ton école.</h5>
+                <h4 class="chubby margin_top">Mais surtout :</h4>
+                <h5 class="din margin_top">Vivre une expérience unique et faire partie de la communauté ANEO!</h5>
+            </div>
+        </div>
+    </div>
+    <br/><br/><br/><br/><br/>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="chubby">Inscris-toi</h1>
+        </div>
+    </div>
+    <br/><br/><br/>
+    <div class="row text-center">
+        <div class="flex-container">
+            <div class="flex-item"><div id="centrerond" class="circle_button"></div></div>
+            <div class="flex-item"><img class="turn-arround-left" src="img/elements/BARRE_CONTOUR_CERCLE.png"/></div>
+            <div class="flex-item"><img class="turn-arround" src="img/elements/BARRE_GRANDE_CONTOUR_CERCLE.png"/></div>
+        </div>
+        <a class="chubby"><h4>Pour <br/> participer</h4></a>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h5 class="din">Envoie ta candidature à</h5>
+            <h4 class="din">ambassador@aneo.fr</h4>
+            <h5 class="din">Avant le 31 Janvier 2017.</h5>
+        </div>
+    </div>
+    <br/><br/><br/><br/><br/>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="chubby">Points Ambassadeurs</h1>
+        </div>
+    </div>
+    <br/><br/><br/>
+    <div class="row text-center">
+        <div style="height: 250px;" class="col-md-4 col-md-offset-2 darkgrey_background">
+            <div>
+                <h3 class="chubby">Contacts</h3>
+                <h5 class="din margin_top">122 avenue du Général Leclerc <br/>Boulogne-Billancourt<br/><br/>01 46 44 24 66
+                    <br/><br/> contact@aneo.fr</h5>
+            </div>
+        </div>
+        <div style="height: 250px;" class="col-md-4 darkgrey_background">
+            <h3 class="chubby">Retrouvez nous sur</h3>
+            <h5 class="din margin_top"><a href="http://www.aneo.eu">www.aneo.eu</a></h5>
+            <a href="https://twitter.com/aneoconseil?lang=fr" target="_blank"><img class="icone" src="img/elements/TWITTER.png"/></a>
+            <a href="https://www.facebook.com/ANEOConseil/?fref=ts" target="_blank"><img class="icone" src="img/elements/FACEBOOK.png"/></a>
+            <a href="https://fr.linkedin.com/company/aneo" target="_blank"><img class="icone" src="img/elements/LINKEDIN.png"/></a>
+            <a href="http://fr.viadeo.com/fr/company/aneo" target="_blank"><img class="icone" src="img/elements/VIADEO.png"/></a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <iframe src="https://www.google.com/maps/d/embed?mid=1Ag3eJmQfO6zShBjIH_3uJHPe6w0" style="padding:0px!important;" width="100%" height="500"></iframe>
+        </div>
+    </div>
+    <br/><br/><br/><br/><br/><br/>
+    <div class="row">
+        <div class="col-md-2 col-md-offset-5 text-center">
+            <img class="img-responsive" src="img/elements/ANEO_LOGO-01.png" alt="ANEO"/>
+        </div>
+    </div>
+    <br/><br/><br/><br/><br/><br/>
+
+
+</div>
+</body>
+</html>
+<script>
+    $('#programme_colleft').hover(function () {
+        jQuery(this).animate({
+            backgroundColor: "#EC9306"
+        }, 200);
+        jQuery('#title_programme_colleft').animate({
+            color: "#303030"
+        }, 200);
+        jQuery('#bloctitle_programme_colleft').animate({
+            backgroundColor: "White"
+        }, 200);
+    }, function () {
+        jQuery(this).animate({
+            backgroundColor: "transparent"
+        }, 200);
+        jQuery('#title_programme_colleft').animate({
+            color: "white"
+        }, 200);
+        jQuery('#bloctitle_programme_colleft').animate({
+            backgroundColor: "transparent"
+        }, 200);
+    });
+
+    $('#programme_colright').hover(function () {
+        jQuery(this).animate({
+            backgroundColor: "#EC9306"
+        }, 200);
+        jQuery('#title_programme_colright').animate({
+            color: "#303030"
+        }, 200);
+        jQuery('#bloctitle_programme_colright').animate({
+            backgroundColor: "White"
+        }, 200);
+    }, function () {
+        jQuery(this).animate({
+            backgroundColor: "transparent"
+        }, 200);
+        jQuery('#title_programme_colright').animate({
+            color: "white"
+        }, 200);
+        jQuery('#bloctitle_programme_colright').animate({
+            backgroundColor: "transparent"
+        }, 200);
+    });
+
+</script>
